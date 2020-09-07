03b23474b8da0cedfc26139a1f0f1df2da4fef30
zhongyic00 <zhong200234@outlook.com>
2020-09-07 20:09:30 +0800

    bug fix: 修复标题栏按钮不居中bug

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,79 +5,84 @@
 
 DTK_USE_NAMESPACE
 
-MainWindow::MainWindow(Daemon *d, QWidget *parent) : DMainWindow(parent), daemon(d), modified(false) {
-    setMinimumSize(800, 800);
+MainWindow::MainWindow(QWidget *parent) : DMainWindow(parent), modified(false) {
+	setMinimumSize(800, 800);
 
-    mainLayout = new QHBoxLayout(this);
-    notesListView = new ZList(daemon, this);
-    notesListView->setObjectName("List");
-    notesListView->setFixedWidth(300);
-    noteEditView = new Editor(this);
-    noteEditView->setObjectName("Editor");
-    auto spliter = new DVerticalLine(this);
-    spliter->setLineWidth(2);
-    spliter->setFixedWidth(6);
-    mainLayout->addWidget(notesListView);
-    mainLayout->addWidget(spliter);
-    mainLayout->addWidget(noteEditView);
-    mainLayout->addSpacing(5);
+	mainLayout = new QHBoxLayout(this);
+	notesListView = new ZList(this);
+	notesListView->setObjectName("List");
+	notesListView->setFixedWidth(300);
+	noteEditView = new Editor(this);
+	noteEditView->setObjectName("Editor");
+	auto spliter = new DVerticalLine(this);
+	spliter->setLineWidth(2);
+	spliter->setFixedWidth(6);
+	mainLayout->addWidget(notesListView);
+	mainLayout->addWidget(spliter);
+	mainLayout->addWidget(noteEditView);
+	mainLayout->addSpacing(5);
 
-    connect(notesListView, &ZList::currentChanged, [this](const QModelIndex &item) {
-        noteEditView->blockSignals(true);
-        noteEditView->display(item.data(Qt::UserRole).value<ZNote>().getHtml());
-        noteEditView->blockSignals(false);
-    });
-    connect(noteEditView, &Editor::contentChanged, [this](const pss val) {
-        notesListView->setCurrentOverview(val.first);
-        notesListView->setCurrentHtml(val.second);
-        modified = true;
-    });
-    connect(notesListView, &ZList::listEmptied, [this]() { reset(); });
+	connect(notesListView, &ZList::currentChanged, [this](const QModelIndex &item) {
+		noteEditView->blockSignals(true);
+		noteEditView->display(item.data(Qt::UserRole).value<ZNote>().getHtml());
+		noteEditView->blockSignals(false);
+	});
+	connect(noteEditView, &Editor::contentChanged, [this](const pss val) {
+		notesListView->setCurrentOverview(val.first);
+		notesListView->setCurrentHtml(val.second);
+		modified = true;
+	});
+	connect(notesListView, &ZList::listEmptied, [this]() { reset(); });
 
-    initNotesListView();
+	initNotesListView();
 
-    titlebar()->setTitle("深度便笺");
-    titlebar()->setIcon(QIcon(":/images/logo256"));
-    titlebar()->setFixedHeight(40);
-    auto *globalSearchBox = new SearchWidget(titlebar());
-    globalSearchBox->setObjectName("searchBox");
-    //    globalSearchBox->setSizePolicy(QSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding));
-    globalSearchBox->setMaximumWidth(800);
-    globalSearchBox->setSourceModel(notesListView->getModel());
-    titlebar()->setCustomWidget(globalSearchBox, true);
-    connect(globalSearchBox, &SearchWidget::changeCurrent, notesListView, &ZList::setCurrentIndex);
+	titlebar()->setTitle("深度便笺");
+	titlebar()->setIcon(QIcon(":/images/logo256"));
+	titlebar()->setFixedHeight(40);
+	// adjust titlebar buttons so that they're vertical-centered
+	titlebar()->findChild<QWidget *>("DTitlebarDWindowMaxButton")->setFixedHeight(40);
+	titlebar()->findChild<QWidget *>("DTitlebarDWindowMinButton")->setFixedHeight(40);
+	titlebar()->findChild<QWidget *>("DTitlebarDWindowCloseButton")->setFixedHeight(40);
+	titlebar()->findChild<QWidget *>("DTitlebarDWindowOptionButton")->setFixedHeight(40);
+	auto *globalSearchBox = new SearchWidget(titlebar());
+	globalSearchBox->setObjectName("searchBox");
+	//    globalSearchBox->setSizePolicy(QSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding));
+	globalSearchBox->setMaximumWidth(800);
+	globalSearchBox->setSourceModel(notesListView->getModel());
+	titlebar()->setCustomWidget(globalSearchBox, true);
+	connect(globalSearchBox, &SearchWidget::changeCurrent, notesListView, &ZList::setCurrentIndex);
 
-    QWidget *hiddenLayer = new QWidget(this);
-    hiddenLayer->setLayout(mainLayout);
+	QWidget *hiddenLayer = new QWidget(this);
+	hiddenLayer->setLayout(mainLayout);
 
-    mainLayout->setContentsMargins(0, 5, 0, 0);
-    this->setCentralWidget(hiddenLayer);
-    QStatusBar *stat = statusBar();
-    stat->setObjectName("statusBar");
-    stat->setFixedHeight(15);
+	mainLayout->setContentsMargins(0, 5, 0, 0);
+	this->setCentralWidget(hiddenLayer);
+	QStatusBar *stat = statusBar();
+	stat->setObjectName("statusBar");
+	stat->setFixedHeight(15);
 
-    auto saveAction = new QAction(tr("&Save"));
-    saveAction->setShortcut(QKeySequence::Save);
-    connect(saveAction, &QAction::triggered, this, &MainWindow::save);
-    addAction(saveAction);
+	auto saveAction = new QAction(tr("&Save"));
+	saveAction->setShortcut(QKeySequence::Save);
+	connect(saveAction, &QAction::triggered, this, &MainWindow::save);
+	addAction(saveAction);
 }
 MainWindow::~MainWindow() {
 }
 void MainWindow::initNotesListView() {
-    //如果没有创建过的note，自动新建
-    reset();
+	//如果没有创建过的note，自动新建
+	reset();
 }
 void MainWindow::save() {
-    if (!modified)
-        return;
-    notesListView->commitChange();
-    daemon->save();
-    modified = false;
+	if (!modified)
+		return;
+	notesListView->commitChange();
+	Daemon::instance()->save();
+	modified = false;
 }
 void MainWindow::reset() {
-    noteEditView->reset();
+	noteEditView->reset();
 }
 void MainWindow::closeEvent(QCloseEvent *e) {
-    e->ignore();
-    hide();
+	e->ignore();
+	hide();
 }
