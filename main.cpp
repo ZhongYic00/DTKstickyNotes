705efa2b01ba0ca6db9d4603bcb0cc1c0a170d72
zhongyic00 <zhong200234@outlook.com>
2020-09-08 23:59:32 +0800

    添加log记录

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
 #include "systemtray.h"
 #include <DAboutDialog>
 #include <DApplication>
+#include <DLog>
 
 DWIDGET_USE_NAMESPACE
-
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+	QByteArray localMsg = msg.toLocal8Bit();
+	switch (type) {
+		case QtDebugMsg:
+			fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+			break;
+		case QtInfoMsg:
+			fprintf(stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+			break;
+		case QtWarningMsg:
+			fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+			break;
+		case QtCriticalMsg:
+			fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+			break;
+		case QtFatalMsg:
+			fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+			break;
+	}
+}
 int main(int argc, char *argv[]) {
 	DApplication::loadDXcbPlugin();
 	DApplication app(argc, argv);
+
+	qInstallMessageHandler(myMessageOutput);
+	std::freopen((QStandardPaths::writableLocation(QStandardPaths::CacheLocation) + "/stickyNotes.log").toStdString().c_str(), "a", stderr);
+	std::cerr << std::endl << std::endl;
 
 	app.setAttribute(Qt::AA_UseHighDpiPixmaps);
 	if (!app.setSingleInstance("DStickyNotes-develop"))
