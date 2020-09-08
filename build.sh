728ea333143e172747365f4289b69e1caa3c5c14
zhongyic00 <zhong200234@outlook.com>
2020-09-08 23:37:21 +0800

    version update

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cp /home/zyc/Github/StickyNotes-build/build-StickyNotes-unknown-Release/StickyNotes /home/zyc/Github/DTKstickyNotes/build/opt/apps/DTKstickyNotes/bin/
-VERSION=0.0.3-beta
+VERSION=0.0.4-beta
 dpkg -b build/ DTKstickyNotes-$VERSION-amd64.deb
